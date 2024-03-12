fb17ecdfe9dd51855d414808fb6f32593b78faea
laudkyle <kyleaby1@gmail.com>
2024-03-12 19:57:35 +0000

    Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,43 @@
 import { motion } from "framer-motion";
 import styled from "styled-components";
-import { Splide,SplideSlide } from "@splidejs/react-splide";
-import React, { useState, useEffect } from 'react';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import React, { useState, useEffect } from "react";
 
 function Home() {
-    // React component
+  // React component
 
-const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-useEffect(() => {
-  // Fetch data from the backend API when the component mounts
-  fetch('http://backend.addlifetoyears.org/')
-    .then(response => response.json())
-    .then(result => setData(result))
-    .catch(error => console.error('Error fetching data:', error));
+  useEffect(() => {
+    // Fetch data from the backend API when the component mounts
+    fetch("https://backend.addlifetoyears.org/")
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
-    console.log(data)
-}, []);
   return (
     <motion.div
       animate={{ opacity: 1 }}
       initial={{ opacity: 0 }}
       exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}>
-    <Container>
-    <Splide
-    options={{
-        perPage: 1,
-          arrows: false,
-          pagination: false,
-          drag: "free",
-          gap: "5rem",
-    }}>
-        {data.map((item) => {
-            <div key={item.id}>
-            <h4>{"https://addlifetoyears.org/" + item.img}</h4>
-            </div>
-        })}
-    </Splide>
-
-    </Container>
+      transition={{ duration: 0.5 }}
+    >
+      <Container>
+        {data.map((item) => (
+          <div key={item.key}>
+            <img src={"https://addlifetoyears.org/"+item.img} alt={item.img} />
+          </div>
+        ))}
+      </Container>
     </motion.div>
   );
 }
+
 const Container = styled.div`
-    width:96%;
+  width: 96%;
 `;
+
 export default Home;
