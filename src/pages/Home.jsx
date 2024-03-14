e6b46bbaea3ce2f6c15af2f7767e2e24e3b3ac04
laudkyle <kyleaby1@gmail.com>
2024-03-14 18:26:36 +0000

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,10 +25,10 @@ function Home() {
       initial={{ opacity: 0 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
-      className="text-[#fa6cd6]"
+      className="text-[#f85cd1]"
     >
-      <div className="w-full max-w-[1240px] mx-auto h-screen text-center flex flex-col justify-center mt-[-96px]">
-        <p className="text-2xl font-bold p-2 capitalize">you must survive</p>
+      <div className="w-full max-w-[1240px] sm:mx-auto h-screen text-center flex flex-col justify-center mt-[-96px]">
+        <p className="text-2xl font-bold p-2 capitalize">You must survive</p>
         <h1 className="md:text-6xl sm:text-5xl text-3xl font-bold md:py-6 text-[#8e24aa]">Cancer is no death sentence</h1>
         <h2 className="md:text-4xl sm:text-3xl text-xl font-bold">We will fight with you</h2>
       </div>
