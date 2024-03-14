ab6090a44b1cbc5f4903a0b4d16031911e145181
Laudkyle <kyleaby1@gmail.com>
2024-03-14 17:04:44 +0000

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import { motion } from "framer-motion";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import React, { useState, useEffect } from "react";
+import "@splidejs/react-splide/css";
+
 
 function Home() {
   // React component
@@ -10,7 +12,7 @@ function Home() {
 
   useEffect(() => {
     // Fetch data from the backend API when the component mounts
-    fetch("https://backend.addlifetoyears.org/")
+    fetch("http://backend.addlifetoyears.org")
       .then((response) => response.json())
       .then((result) => {
         setData(result);
@@ -25,24 +27,27 @@ function Home() {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <div className="w-full h-screen mt-[-96px]">
+      <div className="mt-1 m-6 z-0">
         <Splide
           options={{
             perPage: 1,
             arrows: false,
             pagination: false,
-            drag: "free",
+            gap:"2rem",
           }}
         >
           {data.map((item) => (
             <SplideSlide key={item.key}>
+            <div className="overflow-hidden rounded-md h-[50%]"></div>
               <img
                 src={"https://addlifetoyears.org/" + item.img}
                 alt={item.img}
-                className="w-full h-full"
+                className="w-[100%] h-[100%] object-cover left-0"
               />
             </SplideSlide>
+
           ))}
+
         </Splide>
       </div>
     </motion.div>
