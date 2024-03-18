c7f708f8af772492c0fca6270b49715b3936d9ea
laudkyle <kyleaby1@gmail.com>
2024-03-18 18:30:35 +0000

    Newsletter

---
+++ src/components/Info.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='text-white w-full h-screen bg-[#5c007a]'>
+      
+    </div>
+  )
+}
+
+export default Info
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import Mission from '../components/Mission'
+import React from 'react';
+import Hero from '../components/Hero';
+import Mission from '../components/Mission';
+import Newsletter from '../components/Newsletter';
+import Info from '../components/Info';
 
 const Home = () => {
   return (
     <div>
     <Hero />
     <Mission />
+    <Newsletter />
+    <Info />
       
     </div>
   )
