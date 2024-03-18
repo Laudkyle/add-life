1b83ac82a268c82762878ec330f9954f6bb97fde
laudkyle <kyleaby1@gmail.com>
2024-03-18 18:43:06 +0000

    Newsletter completed

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const Newsletter = () => {
   return (
-    <div className="w-full py-16 text-white px-4">
+    <div className="w-full h-3/4 py-16 bg-white text-[#5c007a] px-4">
       <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
         <div className="lg:col-span-2 my-4">
           <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
-            Want tips & Tricks to optimize your flow?
+             Want Tips & Tricks to optimize your living?
           </h1>
-          <p>Sign up on our Newletter to stay updated.</p>
+          <p>Subscrib to our newsletter to stay updated.</p>
         </div>
         <div className="my-4">
           <div className="flex flex-col sm:flex-row items-center justify-between">
-            <input className="w-full flex p-3 rounded-md text-black" type="email" placeholder="Enter your Email" />
-            <button className="bg-[#00df9a] w-48 py-2 ml-4 my-6 rounded-md font-medium text-[#000300]">
+            <input className="w-full flex p-3 rounded-md text-[#333]" type="email" placeholder="Enter your Email" />
+            <button className="bg-[#8e24aa] w-48 py-2 ml-4 my-6 rounded-md text-white font-medium">
               Notify Me
             </button>
           </div>
-          <p>We care about the protection of your data. Read our <span className="text-[#00df9a]">Privacy Policy</span>. </p>
+          <p>We care about the your health. Read our <span className="text-[#8e24aa]">Privacy Policy</span>. </p>
         </div>
       </div>
     </div>
