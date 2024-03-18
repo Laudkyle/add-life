1659dc7a340382001041c68376216284b26dc3e9
laudkyle <kyleaby1@gmail.com>
2024-03-18 18:22:13 +0000

    Mission complete

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -8,19 +8,63 @@ import "@splidejs/react-splide/css";
 const Mission = () => {
   return (
     <div className="w-full h-screen py-8">
-      <div className="font-bold md:text-2xl sm:text-2xl text:xl text-center max-w-[250px] mx-auto h-[50px] text-[#5c007a] uppercase">
-        <p>Our misson</p>
+      <div className="font-semibold md:text-4xl sm:text-3xl text:2xl text-center max-w-[120px] sm:max-w-[200px] md:max-w-[250px] mx-auto h-[50px] text-[#5c007a] uppercase">
+        <p
+        className="border-b-2 sm:border-b-4 border-b-black"
+        >Our misson</p>
       </div>
       <Splide
         options={{
-          perPage: 3,
+          perPage: 4,
           arrows: false,
           pagination: false,
           gap: "2rem",
-          autoplay: true,
         }}
       >
-        <div className="flex justify-center items-center gap-x-16 h-screen mt-[-50px]">
+        <div className="w-full flex justify-center items-center gap-x-12 h-screen mt-[-50px]">
+         
+          <SplideSlide>
+            <div className="w-[300px] h-[420px] cursor-pointer group perspective bg-transparent">
+              <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
+                <div className="absolute border-2 w-full h-full backface-hidden">
+                  <img src={img3} className="h-full w-full" alt="img1" />
+                </div>
+                <div className="absolute w-full h-full my-rotate-y-180 backface-hidden bg-gray-400">
+                  <div className="text-center flex flex-col items-center justify-center h-full px-2 pb-24">
+                    <h1 className="text-3xl font-semibold ">Misson 1</h1>
+                    <p className="my-2">Support</p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quos itaque similique hic aperiam delectus quaerat eius
+                      excepturi atque? Accusantium sit nisi dolores dolore,
+                      officiis asperiores eos laudantium. Facere modi id.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SplideSlide>
+          <SplideSlide>
+            <div className="w-[300px] h-[420px] cursor-pointer group perspective bg-transparent">
+              <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
+                <div className="absolute border-2 w-full h-full backface-hidden">
+                  <img src={img1} className="h-full w-full" alt="img1" />
+                </div>
+                <div className="absolute w-full h-full my-rotate-y-180 backface-hidden bg-gray-400">
+                  <div className="text-center flex flex-col items-center justify-center h-full px-2 pb-24">
+                    <h1 className="text-3xl font-semibold ">Misson 3</h1>
+                    <p className="my-2">Support</p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Quos itaque similique hic aperiam delectus quaerat eius
+                      excepturi atque? Accusantium sit nisi dolores dolore,
+                      officiis asperiores eos laudantium. Facere modi id.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </SplideSlide>
           <SplideSlide>
             <div className="w-[300px] h-[420px] cursor-pointer group perspective bg-transparent">
               <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
@@ -85,7 +129,7 @@ const Mission = () => {
             </div>
           </SplideSlide>
           <SplideSlide>
-            <div className="w-[300px] h-[420px] cursor-pointer group perspective bg-transparent">
+            <div className="w-[300px] h-[400px] cursor-pointer group perspective bg-transparent">
               <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
                 <div className="absolute border-2 w-full h-full backface-hidden">
                   <img src={img3} className="h-full w-full" alt="img1" />
@@ -93,48 +137,6 @@ const Mission = () => {
                 <div className="absolute w-full h-full my-rotate-y-180 backface-hidden bg-gray-400">
                   <div className="text-center flex flex-col items-center justify-center h-full px-2 pb-24">
                     <h1 className="text-3xl font-semibold ">Misson 1</h1>
-                    <p className="my-2">Support</p>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Quos itaque similique hic aperiam delectus quaerat eius
-                      excepturi atque? Accusantium sit nisi dolores dolore,
-                      officiis asperiores eos laudantium. Facere modi id.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="w-[300px] h-[420px] cursor-pointer group perspective bg-transparent">
-              <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
-                <div className="absolute border-2 w-full h-full backface-hidden">
-                  <img src={img3} className="h-full w-full" alt="img1" />
-                </div>
-                <div className="absolute w-full h-full my-rotate-y-180 backface-hidden bg-gray-400">
-                  <div className="text-center flex flex-col items-center justify-center h-full px-2 pb-24">
-                    <h1 className="text-3xl font-semibold ">Misson 1</h1>
-                    <p className="my-2">Support</p>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Quos itaque similique hic aperiam delectus quaerat eius
-                      excepturi atque? Accusantium sit nisi dolores dolore,
-                      officiis asperiores eos laudantium. Facere modi id.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <div className="w-[300px] h-[420px] cursor-pointer group perspective bg-transparent">
-              <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
-                <div className="absolute border-2 w-full h-full backface-hidden">
-                  <img src={img1} className="h-full w-full" alt="img1" />
-                </div>
-                <div className="absolute w-full h-full my-rotate-y-180 backface-hidden bg-gray-400">
-                  <div className="text-center flex flex-col items-center justify-center h-full px-2 pb-24">
-                    <h1 className="text-3xl font-semibold ">Misson 3</h1>
                     <p className="my-2">Support</p>
                     <p>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
