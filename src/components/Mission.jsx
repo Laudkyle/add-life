a634ec32e4173ed41e96c75892dc72b177593c43
Laudkyle <kyleaby1@gmail.com>
2024-03-18 16:56:52 +0000

    changes

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -8,14 +8,14 @@ import "@splidejs/react-splide/css";
 const Mission = () => {
   return (
     <div className="w-full h-screen py-8">
-      <div className="font-bold md:text-2xl sm:text-2xl text:xl text-center max-w-[250px] mx-auto  h-[50px] text-[#5c007a] uppercase">
+      <div className="font-bold md:text-2xl sm:text-2xl text:xl text-center max-w-[250px] mx-auto h-[50px] text-[#5c007a] uppercase">
         <p>Our misson</p>
       </div>
       <Splide options={{
           arrows: false,
           pagination: false,
-          drag: "free",
           gap: "2rem",
+          autoplay:true
         }}>
         <div className="flex justify-center items-center gap-x-16 h-screen mt-[-50px]">
           <SplideSlide>
@@ -123,7 +123,7 @@ const Mission = () => {
               </div>
             </div>
           </SplideSlide>
-          <SplideSlide>
+          <SplideSlide >
             <div className="w-[300px] h-[420px] cursor-pointer group perspective bg-transparent">
               <div className="relative preserve-3d group-hover:my-rotate-y-180 w-full h-full duration-1000">
                 <div className="absolute border-2 w-full h-full backface-hidden">
@@ -131,7 +131,7 @@ const Mission = () => {
                 </div>
                 <div className="absolute w-full h-full my-rotate-y-180 backface-hidden bg-gray-400">
                   <div className="text-center flex flex-col items-center justify-center h-full px-2 pb-24">
-                    <h1 className="text-3xl font-semibold ">Misson 1</h1>
+                    <h1 className="text-3xl font-semibold ">Misson 3</h1>
                     <p className="my-2">Support</p>
                     <p>
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
