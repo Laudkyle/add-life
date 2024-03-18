bda94f129b672f84a3a45b591a6892986b292a90
laudkyle <kyleaby1@gmail.com>
2024-03-18 14:53:27 +0000

    changes

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const Mission = () => {
   return (
-    <div className="w-full h-screen bg-white py-12">
-      <div className=" text-center max-w-[250px] capitalize mx-auto">Our misson</div>
+    <div className="w-full h-screen bg-white py-8">
+      <div className="flex items-center justify-center font-bold md:text-2xl sm:text-2xl text:xl text-center max-w-[250px] mx-auto text-[#5c007a] uppercase">
+        <p>Our misson</p>
+        <div className="flex justify-center items-center gap-x-16">
+          <div className="w-[300px] h-[420px] cursor-pointer bg-transparent">
+            <div className="relative w-full h-full duration-1000">
+              <div className="">
+                <img src="../" alt="" />
+              </div>
+              <div className=""></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
