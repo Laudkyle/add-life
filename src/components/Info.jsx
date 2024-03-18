a3280baaa07dc4c4515b635a882bdc80234fae11
Laudkyle <kyleaby1@gmail.com>
2024-03-18 18:53:46 +0000

    Info page

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Info = () => {
   return (
-    <div className='text-white w-full h-screen bg-[#5c007a]'>
-      
+    <div className="text-white w-full h-screen bg-[#5c007a] py-10">
+      <div className="text-center">
+        <h1 className="text-4xl leading-normal mx-auto mb-12">
+          Information & Support
+        </h1>
+        <div className="flex max-w-5xl mx-auto gap-8">
+          <div className="bg-white/10 p-8 rounded-xl "></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
