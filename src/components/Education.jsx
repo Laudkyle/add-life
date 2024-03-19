e4c41e2a55f0451e8f92e64ed6141299ff6f569c
Laudkyle <kyleaby1@gmail.com>
2024-03-19 07:19:06 +0000

    A few changes

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Education = () => {
   return (
     <div className="w-full h-3/4 py-16 bg-white text-[#5c007a] px-4">
-      <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
-        <div className="lg:col-span-2 my-4">
+      <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3 ">
+        <div className="lg:col-span-2 my-2 ">
           <h1 className="md:text-4xl sm:text-3xl text-2xl py-2">
-             Want Tips & Tricks to optimize your living?
+             Support Matters
           </h1>
-          <p>Subscribe to our newsletter to stay updated.</p>
+          <p className="md:text-2xl sm:text-xl">"Patients need someone to hope and support them emotionally. No one fights cancer alone. If you want to fight this fight, I will fight with you!!!"</p>
         </div>
         <div className="my-4">
           <div className="flex flex-col sm:flex-row items-center justify-between">
