deaf921fbcc8ed31ddc453a83191ce9e352d2f47
Laudkyle <kyleaby1@gmail.com>
2024-03-26 18:38:29 +0000

    updates

--- src/components/Hope.jsx
+++ src/components/Hope.jsx
@@ -4,11 +4,11 @@ const Hope = () => {
   return (
     <div className='w-full h-[50%] my-16 mx-auto'>
         <div className='text-[#8e24aa] max-w-[1000px] mx-auto'>
-            <h1 className='text-[#4c1c5c] md:text-5xl sm:text-4xl text-3xl capitalize text-center my-8'>There is hope</h1>
+            <h1 className='text-[#4c1c5c] md:text-5xl sm:text-4xl text-3xl capitalize text-center my-8 font-bold'>There is hope</h1>
             <p className='text-center md:text-4xl sm:text-3xl text-2xl'>Patients need someone to hope and support them emotionally. No one
             fights cancer alone. If you want to fight this fight, I will fight
             with you!!!</p>
-            <h3 className='text-center my-4'><strong>Source - </strong> Betty</h3>
+            <h3 className='text-center my-4 sm:text-2xl text-xl'>Source - <strong>Betty</strong></h3>
         </div>
       
     </div>
