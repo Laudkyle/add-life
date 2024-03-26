256859dc6fe12ea39bba0ecca341b177e4543a20
laudkyle <kyleaby1@gmail.com>
2024-03-26 18:34:48 +0000

    testimony

--- src/components/Hope.jsx
+++ src/components/Hope.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Hope = () => {
   return (
-    <div className='w-full h-screen my-8'>
-        <div className='text-[#8e24aa] w-full mx-auto'>
-            <h1 className='text-[#4c1c5c] md:text-5xl sm:text-4xl text-3xl capitalize text-center '>There is hope</h1>
-            <p>Patients need someone to hope and support them emotionally. No one
+    <div className='w-full h-[50%] my-16 mx-auto'>
+        <div className='text-[#8e24aa] max-w-[1000px] mx-auto'>
+            <h1 className='text-[#4c1c5c] md:text-5xl sm:text-4xl text-3xl capitalize text-center my-8'>There is hope</h1>
+            <p className='text-center md:text-4xl sm:text-3xl text-2xl'>Patients need someone to hope and support them emotionally. No one
             fights cancer alone. If you want to fight this fight, I will fight
             with you!!!</p>
-            <span className='text-center'><strong>Source - </strong> Betty</span>
+            <h3 className='text-center my-4'><strong>Source - </strong> Betty</h3>
         </div>
       
     </div>
