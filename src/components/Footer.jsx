c7f45158b9699132e1810578d9d9062f2715d1ae
Laudkyle <kyleaby1@gmail.com>
2024-03-27 17:09:29 +0000

    workings

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,23 @@ const Footer = () => {
     return (
         <div className="">
             <div className="p-10 bg-gray-800 text-gray-200">
-                <div className=""></div>
+                <div className="max-w-7xl mx-auto">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+                        <div className="mb-5">
+                           <h4>C</h4> 
+                        </div>
+                        <div className="mb-5">
+
+                        </div>
+                        <div className="mb-5">
+
+                        </div>
+                        <div className="mb-5">
+
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
         </div>
 
