bb0948f63d8e252035992674cc7311003be0df7e
Laudkyle <kyleaby1@gmail.com>
2024-03-27 17:04:44 +0000

    Corrections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <div className="">
-            <div class="p-10 bg-gray-800 text-gray-200">
+            <div className="p-10 bg-gray-800 text-gray-200">
                 <div className=""></div>
             </div>
         </div>
