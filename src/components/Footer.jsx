a06bc628590378c33ec22a910c266000363e6170
Laudkyle <kyleaby1@gmail.com>
2024-03-28 17:20:44 +0000

    Useful links addition

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 const Footer = () => {
     return (
         <div className="w-100 h-[50%]">
@@ -7,21 +7,40 @@ const Footer = () => {
                 <div className="max-w-7xl mx-auto">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
                         <div className="mb-5">
-                           <h4>Compnay Name</h4> 
-                           A123 lost street <br/>
-                           Chadlens PB 112232 <br/>
-                           Ghana <br /><br />
-                           <strong>Phone:</strong> : +233 (0)24 124 3373 <br />
-                           <strong>Email:</strong> : addlifetoyears@gmail.com <br />
+                            <h4>Compnay Name</h4>
+                            A123 lost street <br />
+                            Chadlens PB 112232 <br />
+                            Ghana <br /><br />
+                            <strong>Phone:</strong> : +233 (0)24 124 3373 <br />
+                            <strong>Email:</strong> : addlifetoyears@gmail.com <br />
                         </div>
                         <div className="mb-5">
                             <h4>Useful Links</h4>
-                            <ul>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
-                                <li></li>
+                            <ul className="flex  justify-end items-center">
+                                <li className={"p-2 "}>
+                                    <NavLink to={"/"}>Home</NavLink>
+                                </li>
+                                <li className={"p-2 "}>
+                                    <NavLink to={"/about"}>Cancer</NavLink>
+                                </li>
+                                <li className={"p-2 "}>
+                                    <NavLink to={"/involved"}>Get Involved</NavLink>
+                                </li>
+                                <li className={"p-2 "}>
+                                    <NavLink to={"/support"}>Lymphoma Support</NavLink>
+                                </li>
+                                <li className={"p-2 "}>
+                                    <NavLink to={"/foundation"}>Our Foundation</NavLink>
+                                </li>
+                                <li className={"p-2 "}>
+                                    <NavLink to={"/events"}>News & Events</NavLink>
+                                </li>
+                                <li className={"p-2 "}>
+                                    <NavLink to={"/projects"}>Projects</NavLink>
+                                </li>
+                                <li className={"p-2"}>
+                                    <NavLink to={"/gallery"}>Gallery</NavLink>
+                                </li>
                             </ul>
 
                         </div>
@@ -32,7 +51,7 @@ const Footer = () => {
 
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
