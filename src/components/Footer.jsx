188117f77d6576bc9b49a1d4ecbffd7eb59221e3
Laudkyle <kyleaby1@gmail.com>
2024-03-29 17:55:04 +0000

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Footer = () => {
     return (
         <div className="w-100 h-[50%]">
-            <div className="p-10 bg-purple-900 text-gray-200">
+            <div className="p-10 bg-purple-900 text-gray-100">
                 <div className="max-w-7xl mx-auto">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
                         <div className="mb-5">
