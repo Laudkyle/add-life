8719b84d55fe0670e45d2949a801a0de5b10476b
Laudkyle <kyleaby1@gmail.com>
2024-03-30 05:43:21 +0000

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -57,9 +57,9 @@ const Footer = () => {
                             <p>Join 1,500+ others and never miss out on our health tips and more</p>
                             <div className="my-4">
                                 <div className="flex flex-col sm:flex-row items-center justify-between">
-                                    <input className="w-full flex p-3 rounded-md text-[#333]" type="email" placeholder="Enter your Email" />
-                                    <button className="bg-[#8e24aa] w-48 py-2 ml-4 my-6 rounded-md text-white font-medium">
-                                        Notify Me
+                                    <input className="w-full flex p-2 rounded-md text-[#333]" type="email" placeholder="Enter your Email" />
+                                    <button className="bg-[#8e24aa] w-48 py-2 ml-4 my-6 rounded-md text-white font-medium text-center lg:text-justify">
+                                        Subscribe
                                     </button>
                                 </div>
                             </div>
