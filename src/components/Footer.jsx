bf4c1aa7b808d8379f6d4993449b55f1996fcdce
Laudkyle <kyleaby1@gmail.com>
2024-03-31 05:57:50 +0000

    pastered

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
                                     <NavLink to={"/gallery"}><div className="flex flex-row items-center"><FaChevronRight />Health Talks</div></NavLink>
                                 </li>
                                 <li className={"py-1 "}>
-                                    <NavLink to={"/support"}>Lymphoma Support</NavLink>
+                                    <NavLink to={"/support"}><div className="flex flex-row items-center"><FaChevronRight />Home</div></NavLink>
                                 </li>
 
                             </ul>
