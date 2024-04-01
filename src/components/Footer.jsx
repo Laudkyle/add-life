67da454589fb3da09c409d6ff2c1705a64f6dbf9
Laudkyle <kyleaby1@gmail.com>
2024-04-01 06:13:54 +0000

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
                                 <li className="py-1">
                                     <NavLink to={"/"}><div className="flex flex-row items-center"><FaChevronRight size={10} />Home</div></NavLink>
                                 </li>
-                                <li className={"py-1 "}>
+                                <li className={"py-1 hover:text-4xl"}>
                                     <NavLink to={"/about"}><div className="flex flex-row items-center"><FaChevronRight size={10} />Cancer</div></NavLink>
                                 </li>
                                 <li className={"py-1 "}>
