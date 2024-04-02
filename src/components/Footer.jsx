40066cbb1b796a4d956855da5c9ee19330fe8b0e
Laudkyle <kyleaby1@gmail.com>
2024-04-02 06:33:35 +0000

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => {
                             <div className="my-4">
                                 <div className="flex flex-col sm:flex-row items-center justify-between">
                                     <form className="flex flex-row flex-wrap">
-                                        <input className="w-2/3 flex p-2 rounded-md text-[#333]" type="email" placeholder="email@example.com" />
+                                        <input className="w-2/3 flex p-2 text-[#333]" type="email" placeholder="email@example.com" />
                                         <button className="bg-[#8e24aa] w-1/3 p-2  text-white fnt-medium hover:bg-pink-600">
                                             Subscribe
                                         </button>
